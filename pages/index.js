551637b40ff4c67e36f75fdff0d999b1fa3a1da5
gcgbarbosa <gcgbarbosa@gmail.com>
2021-04-16 13:31:33 -0700

    Add boilerplate

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+    </Container>
+  );
+}
