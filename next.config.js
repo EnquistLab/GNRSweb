e2337c779bba2e705eb14a7538db43d7967da288
gcgbarbosa <gcgbarbosa@gmail.com>
2021-04-16 15:28:40 -0700

    Add configuration file

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingSlash: true,
+  env: {
+    // production api
+    apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8875/gnrs_api.php",
+    // development api
+    // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9875/gnrs_api.php", 
+  },
+};
+
+
