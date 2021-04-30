b60d60ec424fdaddb1d6ff0ac554ad6b23b218f6
gcgbarbosa <gcgbarbosa@gmail.com>
2021-04-30 12:23:20 -0700

    Change the input to use only 3 columns

--- actions/api-requests/request-resolve-names.js
+++ actions/api-requests/request-resolve-names.js
@@ -3,23 +3,27 @@ import axios from "axios";
 const apiEndPoint = process.env.apiEndPoint;
 
 export const requestResolveNames = async (queryNames) => {
-  let arrayNames = queryNames.split('\n')
+  let arrayNames = queryNames
+    .split("\n")
+    // remove empty rows
     .filter((row) => row)
-    .map((rows) => rows.split(',') )
-  // 
+    // split fields and concatenate empty id
+    .map((rows) => [""].concat(rows.split(",")));
+  
+  //
   const parseObject = {
     opts: {
       mode: "resolve",
     },
     data: arrayNames,
   };
-  // 
+  //
   return await axios
     .post(apiEndPoint, parseObject, {
       headers: { "Content-Type": "application/json" },
     })
     .then((response) => {
-      return response.data
+      return response.data;
       //setParsedNames(response.data);
     });
 };
