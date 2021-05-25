9c1baea2ae6d7166a167f6a05573119720c75d09
gcgbarbosa <gcgbarbosa@gmail.com>
2021-05-24 18:58:11 -0700

    Add react-papaparse to the index page

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,51 @@
 import React, { useState } from "react";
 
-import {
-  Layout,
-  SearchBox,
-  ResolveTable,
-} from "../components/";
+import { readString } from "react-papaparse";
+
+import { Layout, SearchBox, ResolveTable } from "../components/";
 import { requestResolveNames } from "../actions/";
 
-import {Typography, Paper, Box} from "@material-ui/core";
+import { Typography, Paper, Box } from "@material-ui/core";
 
 export default function Index() {
   const [resolvedNames, setResolvedNames] = useState([]);
   const [isProcessing, setIsProcessing] = useState(false);
   const [isBadInput, setIsBadInput] = useState(false);
+
   const handleResolveNames = async (names, separator) => {
     // clear the table after submiting
     setResolvedNames([]);
     // show spinner
     setIsProcessing(true);
     setIsBadInput(false);
+
     // split names
+    /*
     let arrayNames = names
       .split("\n")
       // remove empty rows
       .filter((row) => row)
       // split fields and concatenate empty id
         .map((rows) => [""].concat(rows.split(separator)));
+    */
+
+    // use the CSV library to read the names
+    let splitNames = readString(names)["data"];
+    // when we pass it to the API we add one empty column
+    splitNames = splitNames.map((rows) => [""].concat(rows));
 
     // check if all elements have 4 columns
-    if (arrayNames.every((row) => row.length == 4) == false) {
+    if (splitNames.every((row) => row.length == 4) == false) {
       setIsProcessing(false);
       setIsBadInput(true);
       return;
     }
+
     // resolve the names
-    let resolvedNames = await requestResolveNames(arrayNames);
+    let resolvedNames = await requestResolveNames(splitNames);
+
+    // handle errors here?
+
     // console.log(resolvedNames)
     setResolvedNames(resolvedNames);
     // hide spinner
@@ -45,13 +56,13 @@ export default function Index() {
     <Layout>
       <SearchBox onSubmit={handleResolveNames} isProcessing={isProcessing} />
       {isBadInput && (
-      <Box pt={2}>
-        <Paper color='primary' variant='outlined'>
-          <Box m={2}>
-            <Typography>All rows must have 3 columns</Typography>
-          </Box>
-        </Paper>
-      </Box>
+        <Box pt={2}>
+          <Paper color="primary" variant="outlined">
+            <Box m={2}>
+              <Typography>All rows must have 3 columns</Typography>
+            </Box>
+          </Paper>
+        </Box>
       )}
       <ResolveTable tableData={resolvedNames} />
     </Layout>
