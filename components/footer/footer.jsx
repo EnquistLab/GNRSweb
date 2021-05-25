8b3a2649cc693fc22646ae21ccc1d3e290873733
gcgbarbosa <gcgbarbosa@gmail.com>
2021-05-24 18:27:52 -0700

    Add part of ing papa parse

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,16 +1,88 @@
-import { Link, Typography, Box } from "@material-ui/core";
+import React from "react";
+import { Typography, Box, Grid } from "@material-ui/core";
 import { ContainerLG } from "../";
 
-function Copyright() {
+function GNRSFooter() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="#">
-        GNRS
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+    <>
+      <Box display="flex" alignItems="flex-end">
+        <Box>
+          <img src="world.png" height="80"></img>
+        </Box>
+        <Box ml={2}>
+          <Typography variant="h3">GNRS</Typography>
+          <Typography variant="h6">
+            Geographic Name Resolution Service
+          </Typography>
+        </Box>
+        <Box flexGrow={1}></Box>
+        <Box width={1 / 4}>
+          An online tool for the standardization of global political division
+          names.
+        </Box>
+      </Box>
+      <Box mt={6} display='flex'>
+        <Grid container spacing={2} alignItems="center">
+          <Grid xs={4} item>
+            <Box display="flex">
+              <Box flexGrow={1}></Box>
+              <Box>
+                <img src="bien.png" height="50"></img>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid xs={8} item>
+            The Botanical Information and Ecology Network
+          </Grid>
+          <Grid xs={4} item>
+            <Box display="flex">
+              <Box flexGrow={1}></Box>
+              <Box>
+                <img src="uoa.png" height="50"></img>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid xs={8} item>
+            The University of Arizona
+          </Grid>
+          <Grid xs={4} item>
+            <Box display="flex">
+              <Box flexGrow={1}></Box>
+              <Box>
+                <img src="nsf.png" height="50"></img>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid xs={8} item>
+            National Science Foundation
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} alignItems="center">
+          <Grid xs={4} item>
+            <Box display="flex">
+              <Box flexGrow={1}></Box>
+              <Box>
+                <img src="globe.png" height="50"></img>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={8}>
+            GeoNames
+          </Grid>
+          <Grid item xs={4}>
+            <Box display="flex">
+              <Box flexGrow={1}></Box>
+              <Box>
+                <img src="lines.png" height="50"></img>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={8}>
+            GADM
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 }
 
@@ -19,7 +91,7 @@ export function Footer() {
     <footer>
       <Box py={10} bgcolor="gray">
         <ContainerLG>
-          <Copyright />
+          <GNRSFooter />
         </ContainerLG>
       </Box>
     </footer>
--- pages/index.js
+++ pages/index.js
@@ -7,30 +7,36 @@ import {
 } from "../components/";
 import { requestResolveNames } from "../actions/";
 
+import {Typography, Paper, Box} from "@material-ui/core";
 
 export default function Index() {
   const [resolvedNames, setResolvedNames] = useState([]);
   const [isProcessing, setIsProcessing] = useState(false);
-
+  const [isBadInput, setIsBadInput] = useState(false);
   const handleResolveNames = async (names, separator) => {
     // clear the table after submiting
     setResolvedNames([]);
     // show spinner
     setIsProcessing(true);
+    setIsBadInput(false);
     // split names
     let arrayNames = names
       .split("\n")
       // remove empty rows
       .filter((row) => row)
       // split fields and concatenate empty id
-      .map((rows) => [""].concat(rows.split(separator)));
+        .map((rows) => [""].concat(rows.split(separator)));
 
+    // check if all elements have 4 columns
+    if (arrayNames.every((row) => row.length == 4) == false) {
+      setIsProcessing(false);
+      setIsBadInput(true);
+      return;
+    }
     // resolve the names
     let resolvedNames = await requestResolveNames(arrayNames);
-
     // console.log(resolvedNames)
     setResolvedNames(resolvedNames);
-
     // hide spinner
     setIsProcessing(false);
   };
@@ -38,6 +44,15 @@ export default function Index() {
   return (
     <Layout>
       <SearchBox onSubmit={handleResolveNames} isProcessing={isProcessing} />
+      {isBadInput && (
+      <Box pt={2}>
+        <Paper color='primary' variant='outlined'>
+          <Box m={2}>
+            <Typography>All rows must have 3 columns</Typography>
+          </Box>
+        </Paper>
+      </Box>
+      )}
       <ResolveTable tableData={resolvedNames} />
     </Layout>
   );
