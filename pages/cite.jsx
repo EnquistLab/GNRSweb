a43b1efb88693c9989a347550670a837202f202b
gcgbarbosa <gcgbarbosa@gmail.com>
2021-06-15 17:42:34 -0700

    Add cite page

--- pages/cite.jsx
+++ pages/cite.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 import { Layout } from "../components/";
-
 import { Typography } from "@material-ui/core";
+import { requestSources } from "../actions/";
 
-export default function Cite() {
+function Cite({ sources }) {
   return (
     <Layout>
       <Typography variant="h3">How to Cite the GNRS</Typography>
+      {JSON.stringify(sources)}
     </Layout>
   );
 }
+
+Cite.getInitialProps = async () => {
+  let sources = await requestSources();
+  return { sources: sources };
+};
+
+export default Cite;
