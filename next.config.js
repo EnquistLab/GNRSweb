be16260c1e55725e26d6d86021f2683019ac696d
gcgbarbosa <gcgbarbosa@gmail.com>
2021-06-28 15:10:58 -0700

    Add the server to the config to load images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   trailingSlash: true,
   env: {
+    apiServer: "http://vegbiendev.nceas.ucsb.edu:9875/", 
     // production api
     // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8875/gnrs_api.php",
     // development api
