2fd07312aabf5875cc5f80b120f340414bddfc42
gcgbarbosa <gcgbarbosa@gmail.com>
2021-06-28 14:14:53 -0700

    Fix the port of the development API

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   trailingSlash: true,
   env: {
     // production api
-    apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8875/gnrs_api.php",
+    // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8875/gnrs_api.php",
     // development api
-    // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9876/gnrs_api.php", 
+    apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9875/gnrs_api.php", 
   },
 };
 
