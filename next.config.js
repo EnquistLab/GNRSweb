17bfdaf3c5b10b8ce484722b7cdcedad7c2a1313
gcgbarbosa <gcgbarbosa@gmail.com>
2021-07-05 11:57:23 -0700

    Add production version of the server

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ module.exports = {
   trailingSlash: true,
   env: {
     apiServer: "http://vegbiendev.nceas.ucsb.edu:9875/", 
-    // production api
-    // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8875/gnrs_api.php",
+   // production api
+    apiEndPoint: "https://gnrsapi.xyz/gnrs_api.php",
     // development api
-    apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9875/gnrs_api.php", 
+    // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9875/gnrs_api.php", 
   },
 };
 
